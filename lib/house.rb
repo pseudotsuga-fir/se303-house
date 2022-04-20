b9099b74561f68fae880ce4237c6bcfae5613b2b
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-04-20 13:53:15 -0700

    add a switch statement to the line function

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,10 @@
 class House
   def line(number)
-    "This is the house that Jack built.\n"
+    case number
+    when 1
+      "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
+    end
   end
 end
