3447dc5de84bfb972075bf1e3781d6efe90a76a6
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-04-21 11:37:04 -0700

    add a fourth verse to the case statment

--- lib/house.rb
+++ lib/house.rb
@@ -7,6 +7,8 @@ class House
       "This is the malt that lay in the house that Jack built.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
+    when 4
+      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 end
