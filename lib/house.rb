6e06d83a34720bc3c55e2a945ac92baf13fdcf3e
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-04-21 11:36:48 -0700

    add a third verse to the case statment

--- lib/house.rb
+++ lib/house.rb
@@ -5,6 +5,8 @@ class House
       "This is the house that Jack built.\n"
     when 2
       "This is the malt that lay in the house that Jack built.\n"
+    when 3
+      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 end
