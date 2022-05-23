9d9f60c4ab7ac036c9880b5213a61f7d7ae62aad
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-05-23 14:59:49 -0700

    extract verses 1-7 into verses hash

--- lib/house.rb
+++ lib/house.rb
@@ -7,26 +7,32 @@ class House
       10 => "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
       9 =>  "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
       8 =>  "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-
+      7 =>  "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      6 =>  "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      5 =>  "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      4 =>  "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+      3 =>  "This is the rat that ate the malt that lay in the house that Jack built.\n",
+      2 =>  "This is the malt that lay in the house that Jack built.\n",
+      1 =>  "This is the house that Jack built.\n",
     }
   end
 
   def line(number)
     case number
     when 1
-      "This is the house that Jack built.\n"
+      @verses[1]
     when 2
-      "This is the malt that lay in the house that Jack built.\n"
+      @verses[2]
     when 3
-      "This is the rat that ate the malt that lay in the house that Jack built.\n"
+      @verses[3]
     when 4
-      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      @verses[4]
     when 5
-      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      @verses[5]
     when 6
-      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      @verses[6]
     when 7
-      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      @verses[7]
     when 8
       @verses[8]
     when 9
