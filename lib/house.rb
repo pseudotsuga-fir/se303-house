baf4171145070e153a7dbff40cae8cc204850368
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-05-24 18:47:11 -0700

    remove duplication from verses and iterate over verse in line method

--- lib/house.rb
+++ lib/house.rb
@@ -2,24 +2,27 @@ class House
 
   def initialize
     @verses = {
-      12 => "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      11 => "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      10 => "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      9 =>  "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      8 =>  "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      7 =>  "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      6 =>  "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      5 =>  "dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      4 =>  "cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-      3 =>  "rat that ate the malt that lay in the house that Jack built.\n",
-      2 =>  "malt that lay in the house that Jack built.\n",
+      12 => "horse and the hound and the horn that belonged to the ",
+      11 => "farmer sowing his corn that kept the ",
+      10 => "rooster that crowed in the morn that woke the ",
+      9 =>  "priest all shaven and shorn that married the ",
+      8 =>  "man all tattered and torn that kissed the ",
+      7 =>  "maiden all forlorn that milked the ",
+      6 =>  "cow with the crumpled horn that tossed the ",
+      5 =>  "dog that worried the ",
+      4 =>  "cat that killed the ",
+      3 =>  "rat that ate the ",
+      2 =>  "malt that lay in the ",
       1 =>  "house that Jack built.\n",
     }
   end
 
   def line(number)
-    "This is the " <<
-    @verses[number]
+    result = "This is the "
+    number.downto(1) do |n|
+      result << @verses[n]
+    end
+    result
   end
 
   def recite
