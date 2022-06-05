56e12be528cd570d6516cf51e28a695055a0a263
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-06-05 13:16:59 -0700

    add verse argument to House constructor

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,7 @@
 class House
 
-  def initialize(start_phrase="This is the ")
-    @verses = Verses.new.verses
+  def initialize(start_phrase="This is the ", verses=Verses.new.verses)
+    @verses = verses
     @start_phrase = start_phrase
   end
 
